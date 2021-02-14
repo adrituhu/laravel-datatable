d44fcdde8990859b5892ef30105ba2fdc1ee9d59
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-02-15 06:30:40 +0700

    feat: first basic datatable

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        {{$dataTable->table()}}
+    </div>
+@endsection
+
+@push('scripts')
+    {{$dataTable->scripts()}}
+@endpush
